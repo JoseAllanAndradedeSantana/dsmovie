65264d99bac8d24ff438f0a8bca3916250912e2c
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2022-01-16 12:48:03 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -22,16 +22,20 @@ function Listing() {
     });
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}$sort=title`).then(response => {
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`).then(response => {
             const data = response.data as MoviePage;
             //console.log(data);
             //setPageNumber(data.number);
             setPage(data);
         });
     }, [pageNumber]);
+
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+   }
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange} />
             <div className="container">
                 <div className="row">
                     {page.content.map(movie => (
