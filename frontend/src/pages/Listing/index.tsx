4ceddd9d762e27a43427bcaed45f51cd16965610
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2022-01-16 11:50:04 -0300

    UseParams

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -22,16 +22,13 @@ function Listing() {
     });
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`).then(response => {
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}$sort=title`).then(response => {
             const data = response.data as MoviePage;
             //console.log(data);
             //setPageNumber(data.number);
             setPage(data);
         });
     }, [pageNumber]);
-
-
-
     return (
         <>
             <Pagination />
