8654360c071838ff84a6214ebce4990f5fd9b04e
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2022-01-15 21:14:06 -0300

    Frontend

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -1,6 +1,4 @@
 import { ReactComponent as StarFull} from 'assets/img/star-full.svg';
-import { ReactComponent as StarHalf} from 'assets/img/star-half.svg';
-import { ReactComponent as StarEmpty} from 'assets/img/star-empty.svg';
 import './style.css';
 
 function MovieStars() {
